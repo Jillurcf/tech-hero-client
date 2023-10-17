8f3cd437bab7442c90947d915e3c8c9ee1bbc2ad
jillurcf <jrtushar@gmail.comm>
2023-10-17 15:42:37 -0700

    add product fom created with 8 input field

--- src/Components/AddProduct/AddProcut.jsx
+++ src/Components/AddProduct/AddProcut.jsx
@@ -1,6 +1,26 @@
+import { useRef, useState } from "react";
 
 
 const AddProcut = () => {
+  const buttonRef = useRef()
+  const [addbutton, setAddbutton] = useState(null)
+  const handleClick = ()=>{
+    setAddbutton(buttonRef.current.value);
+  }
+
+  const handleAddProduct = e =>{
+    e.preventDefault()
+    const form = e.target;
+    const image = form.image.value;
+    const name = form.name.value;
+    const brandname = form.brandname.value;
+    const type = form.type.value;
+    const price = form.price.value;
+    const shortdescription = form.shortdescription.value;
+    const rating = form.rating.value;
+    const addbuttons = addbutton;
+    console.log(image, name, brandname, type, price, shortdescription, rating, addbuttons  );
+  }
     return (
         <div>
               <div className="hero">
@@ -11,27 +31,108 @@ const AddProcut = () => {
             </h1>
           </div>
           <div className="card shadow-2xl bg-pink-100">
-            <form className="card-body lg:w-[50vw]">
+            <form onSubmit={handleAddProduct} className="card-body lg:w-[50vw]">
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Email</span>
+                  <span className="label-text">Image</span>
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
+                  type="text"
+                  name="image"
+                  placeholder="image"
                   className="input input-bordered"
                   required
                 />
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text">Name</span>
                 </label>
                 <input
-                  type="password"
-                  name="password"
-                  placeholder="password"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Brand Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="brandname"
+                  placeholder="Brand Name"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Type</span>
+                </label>
+                <input
+                  type="text"
+                  name="type"
+                  placeholder="Type"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="text"
+                  name="price"
+                  placeholder="Price"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Short Description</span>
+                </label>
+                <input
+                  type="text"
+                  name="shortdescription"
+                  placeholder="Short description"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Rating</span>
+                </label>
+                <input
+                  type="text"
+                  name="rating"
+                  placeholder="Rating"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Add Button</span>
+                </label>
+                <input
+                  type="button"
+                  value="click Me!"
+                  ref={buttonRef}
+                  onClick={handleClick}
+                  name="addbutton"
+                  placeholder="Add button"
                   className="input input-bordered"
                   required
                 />
